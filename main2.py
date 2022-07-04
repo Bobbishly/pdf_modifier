9d2cbaf7df2419f3e96ff51061fe80b80d896b1d
Bobbishly <ayomiderichard255@gmail.com>
2022-07-04 22:13:14 +0100

    Updating Some Forgotten Stuffs :)

--- main2.py
+++ main2.py
@@ -7,5 +7,5 @@ def pdf_combiner(pdf_list):
     merger = PyPDF2.PdfFileMerger()
     for pdf in pdf_list:
         merger.append(pdf)
-    merger.write('super.pdf')
+    merger.write('pdfs/super.pdf')
 pdf_combiner(inputs)
